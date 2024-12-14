b6c456f3cd9ea42ebbf2208036e2734c7dd46eeb
mossexe <syifasarah1411@gmail.com>
2024-12-14 11:28:17 +0700

    Main Menu

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 int main (){
-  printf ("Hello World");
+  char menu;
+
+  printf ("A. Growth Tracker");
+  printf ("B. Vaccine Tracker");
+  printf ("C. Sleep Tracker");
+  printf ("D. Health Database");
+
+  scanf("%c", &menu)
+
+  switch(menu){
+     case 'A':
+     break;
+
+     case 'B':
+     break;
+
+     case 'C':
+     break;
+
+     case 'D'  :
+     break; 
+
+     default:
+     printf ("Invalid Input! Try Again!")  
+  }
 }
 return 0;
