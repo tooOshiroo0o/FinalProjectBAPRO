ba695a6c37e00d9fd27037bf36c77f1b63e83f63
tooOshiroo0o <rbbnyy@gmail.com>
2024-12-18 19:04:21 +0700

    main.c

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <time.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+
+#define MAX_LOGS 100
+
+typedef struct{
+    time_t timeStart;
+    time_t timeEnd;
+    int durationInmin;
+    char notes[99];
+} sleepLog;
+
+typedef struct {
+    sleepLog logs[MAX_LOGS];
+    int logCount;
+    float sleepTotal; // in hours
+    int ageInMonth; // baby's age in months
+} babySleep;
+
+// Function declarations
+int ageCalculator();
+void sleepTracker();
+void startSleep();
+void endSleep();
+void sleepHistory();
+void sleepSummary();
+
 
 // Define WHO standards for growth tracker
 typedef struct
@@ -51,6 +76,7 @@ int main()
 {
     int ageInMonths = ageCalculator(); // Calculate child's age in months
     char menu;
+    babySleep tracker = {0};
 
     do
     {
@@ -74,7 +100,7 @@ int main()
             break;
 
         case 'C':
-            printf("Sleep Tracker is under development.\n");
+            sleepTracker(&tracker); // call the sleep tracker
             break;
 
         case 'D':
@@ -437,3 +463,129 @@ void HealthDatabase () {
     }
     
 }
+
+// Sleep Tracker function
+void startSleep(babySleep *tracker){
+    if(tracker -> logCount >= MAX_LOGS){
+        printf("Log is more than its capacity!");
+        return;
+    }
+
+    sleepLog *session = &tracker -> logs[tracker -> logCount];
+    session -> timeStart = time(NULL);
+
+    printf("Shhh! The baby starts to sleep. Monitor: %s\n", ctime(&session->timeStart));
+}
+
+void endSleep(babySleep *tracker){
+    if(tracker -> logCount >= MAX_LOGS){
+        printf("No active sleep sessions needs to be monitored.\n");
+        return;
+    }
+
+    sleepLog *session = &tracker -> logs[tracker -> logCount];
+    session -> timeEnd = time(NULL);
+
+    // calculate the duration in minutes
+    session -> durationInmin = (session -> timeEnd - session -> timeStart) / 60;
+
+    // convert from minutes into hours
+    tracker->sleepTotal += (float)session->durationInmin / 60.0;
+
+    printf("Baby is now fresh and awake! Duration: %d\n", session->durationInmin);
+
+    tracker -> logCount++;
+}
+
+void sleepHistory(babySleep *tracker){
+    if (tracker -> logCount == 0){
+        printf("No logs or history of baby sleeping.\n");
+        return;
+    }
+
+    printf("\n==== Baby Sleep History ====\n");
+    for(int i = 0; i < tracker -> logCount; i++){
+        sleepLog *session = &tracker -> logs[i];  
+        printf("Session %d: \n", i+1);
+        printf("   Started: %s\n", ctime(&session -> timeStart));
+        printf("   Ended: %s\n", ctime(&session -> timeEnd));
+        printf("   Duration: %d minutes\n", session->durationInmin);  
+    }
+}
+
+void sleepSummary(babySleep *tracker){
+    int recommHours = 0;  
+
+    if (tracker -> logCount == 0){
+        printf("No sleep data.\n");
+        return;
+    }
+
+    int totalBabySleeps = tracker -> logCount;
+    float avgDuration = tracker -> sleepTotal / totalBabySleeps;
+
+    printf("\n===== Baby Sleep Summary =====\n");
+    printf("Total Sleep Sessions: %d\n", tracker -> logCount);
+    printf("Total Sleep Time: %.2f hours\n", tracker->sleepTotal);
+    printf("Average Sleep Duration: %.2f hours\n", avgDuration);
+
+    // monitoring total sleep hours of the baby
+    if (tracker -> ageInMonth <= 3){         
+        recommHours = 14; 
+        printf("Newborns needs a very long time to sleep!\n");              
+    }       
+    else if (tracker->ageInMonth > 3 && tracker -> ageInMonth <= 12){         
+        recommHours = 12;
+        printf("The baby needs more time to be asleep!\n");    
+    }      
+    else if(tracker->ageInMonth > 12){         
+        recommHours = 10;
+        printf("The baby still needs more time to be asleep!\n");         
+            
+    }  
+    else{             
+        printf("Your baby is now a toddler, but still needs more sleep!\n");         
+    }  
+
+    printf("Recommended Sleep Duration: %d hours\n", recommHours);
+    if (tracker->sleepTotal < recommHours) {  
+        printf("Notice: Total sleep duration is below normal.\nPlease consult to a doctor.\n");
+    } else {
+        printf("Total sleep duration is within the healthy range.\n");
+    }
+}
+
+void sleepTracker(babySleep *tracker){ 
+    int choice;
+
+   while (1){
+    printf("\n--- Baby Sleep Tracker ---\n");
+        printf("1. Start Sleep Session\n");
+        printf("2. End Sleep Session\n");
+        printf("3. View Sleep History\n");
+        printf("4. Sleep Summary\n");
+        printf("5. Exit\n");
+        printf("Choose option: ");
+        scanf("%d", &choice);
+        getchar(); 
+
+    switch(choice){
+        case 1:
+           startSleep(tracker);
+           break;
+        case 2: 
+           endSleep(tracker);
+           break;
+        case 3:
+           sleepHistory(tracker);
+           break;
+        case 4:
+           sleepSummary(tracker);
+           break;
+        case 5:
+           return;
+        default:
+           printf("Invalid option!");
+    }
+   }
+} 
